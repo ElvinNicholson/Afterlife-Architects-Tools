d61568de25e4dc5396c5b470526b041c9f1be209
Elvin Nicholson <Elvin2.Nicholson@live.uwe.ac.uk>
2023-04-15 23:58:22 +0100

    Added cost and tile size fields

--- AfterlifeArchitectsTools/StructureEditor.xaml.cs
+++ AfterlifeArchitectsTools/StructureEditor.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -30,20 +31,86 @@ namespace AfterlifeArchitectsTools
             structureType_ComboBox.SelectedValue = StructureType.Building_Green_T1;
         }
 
+        /// <summary>
+        /// Regex that only allow numbers
+        /// </summary>
+        /// <param name="text">The string component of the TextBox</param>
+        /// <returns></returns>
+        private bool regexNumber(string text)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            return regex.IsMatch(text);
+        }
+
+        /// <summary>
+        /// Structure values JSON file loading
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void structureLoad_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Structure values JSON file saving
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void structureSave_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Triggers when user changes the selected Structure Type
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void structureType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             StructureType selectedType = (StructureType)(structureType_ComboBox.SelectedItem);
             //MessageBox.Show(selectedType.ToString());
+        }
+
+        /// <summary>
+        /// Saves the changes to cost to the JSON file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cost_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Only allow numbers to be input to the cost field
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cost_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = regexNumber(e.Text);
+        }
+
+        /// <summary>
+        /// Saves the changes to tile size to the JSON file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void size_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Only allow numbers to be input to the tile size field
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void size_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = regexNumber(e.Text);
         }
     }
 }
